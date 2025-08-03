5dbe8d54d0e422e2941dea54f59896028a4464a0
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-08-03 11:20:38 +0100

    #4 Update code styling as per insights

--- app/Http/Controllers/Auth/NewPasswordController.php
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -40,17 +40,24 @@ class NewPasswordController extends Controller
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
-        // Here we will attempt to reset the user's password. If it is successful we
-        // will update the password on an actual user model and persist it to the
-        // database. Otherwise we will parse the error and return the response.
+        // Here we will attempt to reset the user's password. If it is
+        // successful we will update the password on an actual user
+        // model and persist it to the database. Otherwise we will
+        // parse the error and return the response.
         $status = Password::reset(
-            $request->only('email', 'password', 'password_confirmation', 'token'),
+            $request->only(
+                'email',
+                'password',
+                'password_confirmation',
+                'token'
+            ),
             fn ($user) => $this->resetPassword($user, $request->password)
         );
 
-        // If the password was successfully reset, we will redirect the user back to
-        // the application's home authenticated view. If there is an error we can
-        // redirect them back to where they came from with their error message.
+        // If the password was successfully reset, we will redirect the user
+        // back to the application's home authenticated view. If there is an
+        // error we can redirect them back to where they came from with their
+        // error message.
         if ($status === Password::PasswordReset) {
             return to_route('login')->with('status', __($status));
         }
--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Auth\Events\Verified;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\RedirectResponse;
 
@@ -11,14 +12,28 @@ class VerifyEmailController extends Controller
     /**
      * Mark the authenticated user's email address as verified.
      */
-    public function __invoke(EmailVerificationRequest $request): RedirectResponse
-    {
+    public function __invoke(
+        EmailVerificationRequest $request
+    ): RedirectResponse {
         if ($request->user()->hasVerifiedEmail()) {
-            return redirect()->intended(route('dashboard', absolute: false).'?verified=1');
+            return redirect()->intended(
+                route('dashboard', absolute: false).'?verified=1'
+            );
         }
 
-        $request->fulfill();
+        if ($request->user()->markEmailAsVerified()) {
+            /** @var \Illuminate\Contracts\Auth\MustVerifyEmail $user */
+            $user = $request->user();
+            event(new Verified($user));
+        }
 
-        return redirect()->intended(route('dashboard', absolute: false).'?verified=1');
+        // Logs::log(Logs::ACTION_VERIFY_USER, [
+        //     'ip' => $request->ip(),
+        //     'user_agent' => $request->userAgent(),
+        // ], $request->user()?->id ?? null);
+
+        return redirect()->intended(
+            route('dashboard', absolute: false).'?verified=1'
+        );
     }
 }
